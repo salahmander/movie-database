01c22209ed887e059adf921ee454ac93118e57b6
salahmander <salahabdo@hotmail.co.uk>
2020-12-03 23:10:44 +0000

    Alphabetize redux index file.

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,15 @@
 import { combineReducers } from "redux";
-import landingReducer from "./reducers/landingReducer/landingReducer";
-import recommendationReducer from "./reducers/recommendationReducer/recommendationReducer";
 
-import moviesListReducer from "./reducers/movieReducer/moviesListReducer/moviesListReducer";
+import landingReducer from "./reducers/landingReducer/landingReducer";
 import movieDetailReducer from "./reducers/movieReducer/movieDetailReducer/movieDetailReducer";
+import moviesListReducer from "./reducers/movieReducer/moviesListReducer/moviesListReducer";
+import recommendationReducer from "./reducers/recommendationReducer/recommendationReducer";
 import tvDetailReducer from "./reducers/movieReducer/tvDetailReducer/tvDetailReducer";
 
 export default combineReducers({
   landing: landingReducer,
-  recommendation: recommendationReducer,
-  moviesList: moviesListReducer,
   movieDetail: movieDetailReducer,
+  moviesList: moviesListReducer,
+  recommendation: recommendationReducer,
   tvDetail: tvDetailReducer,
 });

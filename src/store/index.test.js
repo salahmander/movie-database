02940ebaa05331d6b4b40e6b37c1bc492c1a5cec
salahmander <salahabdo@hotmail.co.uk>
2020-12-04 17:02:20 +0000

    Update redux store index test file to include latest tv state.

--- src/store/index.test.js
+++ src/store/index.test.js
@@ -41,6 +41,11 @@ describe("rootReducer", () => {
         loading: false,
         error: "",
       },
+      latestTv: {
+        latestTv: [],
+        loading: false,
+        error: "",
+      },
     });
   });
 });

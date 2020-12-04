2614ab7b83d3b74c7cba6f9eab7158e11c0e910a
salahmander <salahabdo@hotmail.co.uk>
2020-12-04 13:56:17 +0000

    Create trending action.

---
+++ src/store/actions/trendingAction/trendingAction.js
@@ -0,0 +1,37 @@
+import * as actionTypes from "../actionTypes";
+import axios from "axios";
+import { APIKey, bodyURL } from "../../../constant";
+
+export const setTrendingRequest = () => {
+  return {
+    type: actionTypes.TRENDING_REQUEST,
+  };
+};
+
+export const setTrendingSuccess = (movie) => {
+  return {
+    type: actionTypes.TRENDING_SUCCESS,
+    movie: movie,
+  };
+};
+
+export const setTrendingFail = (error) => {
+  return {
+    type: actionTypes.TRENDING_FAIL,
+    error: error,
+  };
+};
+
+export const fetchTrending = () => {
+  return (dispatch) => {
+    dispatch(setTrendingRequest());
+    return axios
+      .get(`${bodyURL}trending/all/day?api_key=${APIKey}`)
+      .then((response) => {
+        dispatch(setTrendingSuccess(response.data));
+      })
+      .catch((error) => {
+        dispatch(setTrendingFail(error.message));
+      });
+  };
+};

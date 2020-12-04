d7d049cc97b40ac25bfb10e8ce9f058c44b0cccb
salahmander <salahabdo@hotmail.co.uk>
2020-12-04 21:49:41 +0000

    Merge home display component with home container.

--- src/containers/home/Home.js
+++ src/containers/home/Home.js
@@ -2,8 +2,10 @@ import React, { Component } from "react";
 
 import { connect } from "react-redux";
 
-import HomeDisplay from "../../components/home/homeDisplay/HomeDisplay";
+import "./Home.css";
+
 import Spinner from "../../components/UI/spinner/Spinner";
+import SideScrollView from "../../components/sideScroll/sideScrollView/SideScrollView";
 
 import { fetchTrending } from "../../store/actions/trendingAction/trendingAction";
 import { fetchLatestMovie } from "../../store/actions/latestMovieAction/latestMovieAction";
@@ -17,28 +19,57 @@ export class Home extends Component {
   }
 
   render() {
-    const { loading, trending, latestTv, latestMovie } = this.props;
+    const {
+      loadingTrending,
+      loadingLatestMovie,
+      loadingLatestTv,
+      trending,
+      latestTv,
+      latestMovie,
+    } = this.props;
     return (
-      <div className="home-container">
-        {loading ? (
-          <Spinner />
-        ) : (
-          <HomeDisplay
-            trending={trending}
-            nowMovie={latestTv}
-            nowTV={latestMovie}
-          />
-        )}
-      </div>
+      <>
+        {/* Latest trending tv shows and movies*/}
+        <div className="home-display-container">
+          <div className="home-display-trending">
+            <h2 className="home-display-trending-header">Trending</h2>
+            {loadingTrending ? (
+              <Spinner />
+            ) : (
+              <SideScrollView movies={trending} />
+            )}
+          </div>
+          {/* Latest movies out now in Great Britain*/}
+          <div className="home-display-movie">
+            <h2 className="home-display-movie-header">Now Playing (Movies)</h2>
+            {loadingLatestMovie ? (
+              <Spinner />
+            ) : (
+              <SideScrollView movies={latestMovie} media="movie" />
+            )}
+          </div>
+          {/* Latest TV shows currently on the air*/}
+          <div className="home-display-TV">
+            <h2 className="home-display-movie-TV">On The Air (TV)</h2>
+            {loadingLatestTv ? (
+              <Spinner />
+            ) : (
+              <SideScrollView movies={latestTv} media="tv" />
+            )}
+          </div>
+        </div>
+      </>
     );
   }
 }
 
 const mapStateToProps = (state) => ({
-  latestTv: state.latestTv.latestTv,
   latestMovie: state.latestMovie.latestMovie,
+  latestTv: state.latestTv.latestTv,
+  loadingLatestMovie: state.latestMovie.loading,
+  loadingLatestTv: state.latestTv.loading,
+  loadingTrending: state.latestMovie.loading,
   trending: state.trending.trending,
-  loading: state.latestMovie.loading,
 });
 
 export const mapDispatchToProps = (dispatch) => {

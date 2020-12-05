2a804b1519691cb969fc1e9e9c7f7790f7743817
salahmander <salahabdo@hotmail.co.uk>
2020-12-05 13:17:57 +0000

    Add new route for tv detail display.

--- src/router/Router.js
+++ src/router/Router.js
@@ -2,11 +2,12 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 
 import Home from "../containers/home/Home";
-import MovieViewDisplay from "../containers/movies/MovieViewDisplay";
-import MovieDetailDisplay from "../containers/movies/movieDetailDisplay/MovieDetailDisplay";
 import Login from "../containers/account/login/Login";
-import Register from "../containers/account/register/Register";
+import MovieDetailDisplay from "../containers/movies/movieDetailDisplay/MovieDetailDisplay";
+import MovieViewDisplay from "../containers/movies/MovieViewDisplay";
 import pageNotFound from "../components/UI/404page/404page";
+import Register from "../containers/account/register/Register";
+import TvDetailDisplay from "../containers/movies/tvDetailDisplay/TvDetailDisplay";
 
 const Router = () => {
   return (
@@ -15,6 +16,7 @@ const Router = () => {
         <Route exact path="/" component={Home} />
         <Route exact path="/movies/:name" component={MovieViewDisplay} />
         <Route exact path="/movie/:media/:id" component={MovieDetailDisplay} />
+        <Route exact path="/tv/:media/:id" component={TvDetailDisplay} />
         <Route exact path="/register" component={Register} />
         <Route exact path="/login" component={Login} />
         <Route component={pageNotFound} />

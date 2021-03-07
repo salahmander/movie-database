25675e589e810ee2fc38d18c61cd4d141c711237
salahmander <salahabdo@hotmail.co.uk>
2021-03-07 22:13:52 +0000

    Update home container to use react hooks.

--- src/containers/home/Home.js
+++ src/containers/home/Home.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 import "./Home.css";
 
@@ -10,73 +10,53 @@ import { fetchLatestMovie } from "../../store/actions/latestMovieAction/latestMo
 import { fetchLatestTv } from "../../store/actions/latestTvAction/latestTvAction";
 import { fetchTrending } from "../../store/actions/trendingAction/trendingAction";
 
-export class Home extends Component {
-  componentDidMount() {
-    this.props.onFetchLatestMovie();
-    this.props.onFetchLatestTv();
-    this.props.onFetchTrending();
-  }
+const Home = () => {
+  const dispatch = useDispatch();
 
-  render() {
-    const {
-      latestMovie,
-      latestTv,
-      loadingLatestMovie,
-      loadingLatestTv,
-      loadingTrending,
-      trending,
-    } = this.props;
-    return (
-      <>
-        {/* Latest trending tv shows and movies*/}
-        <div className="home-display-container">
-          <div className="home-display-trending">
-            <h2 className="home-display-trending-header">Trending</h2>
-            {loadingTrending ? (
-              <Spinner />
-            ) : (
-              <SideScrollView movies={trending}/>
-            )}
-          </div>
-          {/* Latest movies out now in Great Britain*/}
-          <div className="home-display-movie">
-            <h2 className="home-display-movie-header">Now Playing (Movies)</h2>
-            {loadingLatestMovie ? (
-              <Spinner />
-            ) : (
-              <SideScrollView movies={latestMovie} media="movie" />
-            )}
-          </div>
-          {/* Latest TV shows currently on the air*/}
-          <div className="home-display-TV">
-            <h2 className="home-display-movie-TV">On The Air (TV)</h2>
-            {loadingLatestTv ? (
-              <Spinner />
-            ) : (
-              <SideScrollView movies={latestTv} media="tv" />
-            )}
-          </div>
+  const latestMovieState = useSelector((state) => state.latestMovie);
+  const { latestMovie, loadingLatestMovie } = latestMovieState;
+
+  const latestTvState = useSelector((state) => state.latestTv);
+  const { latestTv, loadingLatestTv } = latestTvState;
+
+  const trendingState = useSelector((state) => state.trending);
+  const { trending, loadingTrending } = trendingState;
+
+  useEffect(() => {
+    dispatch(fetchLatestMovie());
+    dispatch(fetchLatestTv());
+    dispatch(fetchTrending());
+  }, [dispatch]);
+
+  return (
+    <>
+      {/* Latest trending tv shows and movies*/}
+      <div className="home-display-container">
+        <div className="home-display-trending">
+          <h2 className="home-display-trending-header">Trending</h2>
+          {loadingTrending ? <Spinner /> : <SideScrollView movies={trending} />}
         </div>
-      </>
-    );
-  }
-}
-
-const mapStateToProps = (state) => ({
-  latestMovie: state.latestMovie.latestMovie,
-  latestTv: state.latestTv.latestTv,
-  loadingLatestMovie: state.latestMovie.loading,
-  loadingLatestTv: state.latestTv.loading,
-  loadingTrending: state.latestMovie.loading,
-  trending: state.trending.trending,
-});
-
-export const mapDispatchToProps = (dispatch) => {
-  return {
-    onFetchLatestMovie: () => dispatch(fetchLatestMovie()),
-    onFetchLatestTv: () => dispatch(fetchLatestTv()),
-    onFetchTrending: () => dispatch(fetchTrending()),
-  };
+        {/* Latest movies out now in Great Britain*/}
+        <div className="home-display-movie">
+          <h2 className="home-display-movie-header">Now Playing (Movies)</h2>
+          {loadingLatestMovie ? (
+            <Spinner />
+          ) : (
+            <SideScrollView movies={latestMovie} media="movie" />
+          )}
+        </div>
+        {/* Latest TV shows currently on the air*/}
+        <div className="home-display-TV">
+          <h2 className="home-display-movie-TV">On The Air (TV)</h2>
+          {loadingLatestTv ? (
+            <Spinner />
+          ) : (
+            <SideScrollView movies={latestTv} media="tv" />
+          )}
+        </div>
+      </div>
+    </>
+  );
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Home);
+export default Home;
